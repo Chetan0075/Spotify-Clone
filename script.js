e48c82cee205a7a1ec1de8354b22224ba1b1cda3
Chetan Rajput <151898636+Chetan0075@users.noreply.github.com>
2024-06-11 15:44:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ const playMusic = (track, pause = false) => {
 async function displayAlbums() {
     try {
         // Fetch the list of songs/albums
-        let response = await fetch('/songs/');
+        let response = await fetch('./songs/');
         let textResponse = await response.text();
 
         // Parse the response to extract links
